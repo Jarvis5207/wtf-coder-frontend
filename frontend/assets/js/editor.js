3e5c181f9de821269f832bcc5002e9bc8010be8e
Harsh <h77776667777@gmail.com>
2025-08-06 14:09:40 +0530

    Update editor.js

--- frontend/assets/js/editor.js
+++ frontend/assets/js/editor.js
@@ -4,7 +4,7 @@ if (location.pathname.endsWith("editor.html")) {
   require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs' } });
 require(['vs/editor/editor.main'], function () {
   monaco.editor.create(document.getElementById('container'), {
-    value: '',
+    value: '<!-- HTML -->',
     language: 'html',
     theme: 'vs-dark',
     experimental: {
